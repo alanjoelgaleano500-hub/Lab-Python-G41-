8c6bf9ff50fc7810432c944b7a3fbb91aa9fdea4
Alanjoel <alanjoelgaleano500@gmail.com>
2026-07-07 21:29:24 -0300

    Add files via upload

--- Supermercado Python C41.py
+++ Supermercado Python C41.py
@@ -7,6 +7,7 @@ def limpiar():
 
 productos = []
 carrito = []
+total_recaudado = 0
 
 
 def buscar(codigo):
@@ -18,16 +19,38 @@ def buscar(codigo):
 
 def validar():
     while True:
-        codigo = input("Ingrese codigo: ")
+        codigo = input("Ingrese el codigo: ")
         print("\n")
         if len(codigo) != 3 or not codigo.isdigit():
-            print("El código debe tener exactamente 3 dígitos. \n")
+            print("El codigo debe tener exactamente 3 digitos\n")
+            continue
+
+        numero = int(codigo)
+
+        if numero < 1 or numero > 100:
+            print("El codigo debe estar entre 001 y 100\n")
+            continue
+
+        if buscar(codigo) is not None:
+            print("Ese codigo ya existe\n")
+            continue
+
+        return codigo
+
+
+def pedir_codigo():
+    while True:
+        codigo = input("Ingrese el codigo: ")
+        print("\n")
+        if len(codigo) != 3 or not codigo.isdigit():
+            print("El codigo debe tener exactamente 3 digitos\n")
             continue
 
         numero = int(codigo)
         if numero < 1 or numero > 100:
-            print("El código debe estar entre 001 y 100. \n")
+            print("El codigo debe estar entre 001 y 100\n")
             continue
+
         return codigo
 
 
@@ -36,11 +59,11 @@ def reg_precio():
         try:
             precio = float(input("Precio: "))
             if precio <= 0:
-                print("Precio inválido.")
+                print("El Precio es invalido")
             else:
                 return precio
         except ValueError:
-            print("Ingrese un número.")
+            print("Debe ingresar un numero no caracteres")
 
 
 def reg_stock():
@@ -48,16 +71,16 @@ def reg_stock():
         try:
             stock = int(input("Stock: "))
             if stock < 0:
-                print("Stock inválido.")
+                print("El stock es invalido.")
             else:
                 return stock
         except ValueError:
-            print("Ingrese un número entero.")
+            print("Debe ingresar un numero entero")
 
 
 def registrar():
     if len(productos) >= 100:
-        print("No se pueden registrar más productos.")
+        print("No se pueden registrar mas productos")
         return
 
     print("\n------------- REGISTRAR PRODUCTO -------------")
@@ -70,21 +93,23 @@ def registrar():
         "codigo": codigo,
         "nombre": nombre,
         "precio": precio,
-        "stock": stock
+        "stock": stock,
+        "vendidos": 0,
+        "recaudado": 0
     }
 
     productos.append(producto)
-    print("Producto registrado correctamente.")
+    print("Su producto fue registrado correctamente")
 
 
 def mostrar_prod():
     if len(productos) == 0:
-        print("No hay productos.")
+        print("No hay productos")
         return
     print("\nLISTA DE PRODUCTOS\n")
     for producto in productos:
         print(
-            f"Código: {producto['codigo']} | "
+            f"Codigo: {producto['codigo']} | "
             f"Nombre: {producto['nombre']} | "
             f"Precio: ${producto['precio']:.2f} | "
             f"Stock: {producto['stock']}"
@@ -96,37 +121,48 @@ def sig():
     limpiar()
 
 
+def cantidad_en_carrito(codigo):
+
+    cantidad = 0
+
+    for item in carrito:
+        if item["codigo"] == codigo:
+            cantidad += item["cantidad"]
+
+    return cantidad
+
+
 def agre_carri():
-    codigo = validar()
-    producto_encontrado = buscar(codigo)
-    if producto_encontrado is None:
-        print("El Producto no existe.\n")
+    codigo = pedir_codigo()
+    produc_encontrado = buscar(codigo)
+    if produc_encontrado is None:
+        print("El Producto no existe\n")
         return
 
     while True:
         try:
-            cantidad = int(input("Ingrese Cantidad: "))
+            cantidad = int(input("Ingrese la cantidad: "))
         except ValueError:
-            print("Debe ingresar un número entero.\n")
+            print("Debe ingresar un numero entero\n")
             continue
 
         if cantidad <= 0:
-            print("Debe elegir por lo menos una unidad.\n")
+            print("Debe elegir por lo menos una unidad\n")
             continue
 
-        if cantidad > producto_encontrado["stock"]:
-            print("No hay stock suficiente.\n")
-            continue
+        if cantidad + cantidad_en_carrito(codigo) > produc_encontrado["stock"]:
+            print("No hay stock suficiente")
+            break
 
-        producto_encontrado["stock"] -= cantidad
         nuevo_item = {
-            "codigo": producto_encontrado["codigo"],
-            "nombre": producto_encontrado["nombre"],
+            "codigo": produc_encontrado["codigo"],
+            "nombre": produc_encontrado["nombre"],
             "cantidad": cantidad,
-            "precio": producto_encontrado["precio"],
-            "subtotal": producto_encontrado["precio"] * cantidad,
+            "precio": produc_encontrado["precio"],
+            "subtotal": produc_encontrado["precio"] * cantidad,
         }
         carrito.append(nuevo_item)
+        produc_encontrado["stock"] -= cantidad
         break
 
 
@@ -140,7 +176,7 @@ def calcu_total():
 def mostra_carri():
     while True:
         if not carrito:
-            print("El carrito está vacio.")
+            print("Su carrito esta vacio")
             return None
 
         print("----------------------------------------")
@@ -163,26 +199,80 @@ def mostra_carri():
 
 
 def finalizar():
+    global total_recaudado
+
     if not carrito:
-        print("Error: El carrito se encuentra vacio \n")
+        print("Su carrito esta vacio\n")
         return None
 
-    print("----------------------------------------\n")
-    print("########################\n")
-    print("       TICKET     ")
-    print("########################\n")
-    print()
-
-    for item in carrito:
-        print(f"{item['nombre']} | x{item['cantidad']} | {item['subtotal']}")
-        print()
+    print("------------------------------------------------\n")
+    print("################################################")
+    print("#                  TICKET                      #")
+    print("################################################\n")
 
     total = calcu_total()
 
-    print("----------------------------------------\n")
-    print(f"Total a pagar: {total}")
+    for item in carrito:
+
+        producto = buscar(item["codigo"])
+
+        producto["vendidos"] += item["cantidad"]
+        producto["recaudado"] += item["subtotal"]
+
+        print(
+            f"{item['nombre']} | x{item['cantidad']} | ${item['subtotal']}"
+        )
+
+    total_recaudado += total
+
+    print("------------------------------------------------")
+    print(f"Total a pagar: ${total}")
+
     carrito.clear()
-    return None
+
+
+def estadisticas():
+    if len(productos) == 0:
+        print("No hay productos registrados")
+        return
+
+    total_unidades = 0
+
+    mas_vendido = productos[0]
+
+    for producto in productos:
+
+        total_unidades += producto["vendidos"]
+
+        if producto["vendidos"] > mas_vendido["vendidos"]:
+            mas_vendido = producto
+
+    print("\n========== ESTADISTICAS ==========\n")
+    print(f"Productos registrados: {len(productos)}")
+    print(f"Unidades vendidas: {total_unidades}")
+    print(f"Dinero recaudado: ${total_recaudado}")
+    print("\nProducto más vendido")
+    print(f"Nombre: {mas_vendido['nombre']}")
+    print(f"Codigo: {mas_vendido['codigo']}")
+    print(f"Unidades vendidas: {mas_vendido['vendidos']}")
+    print("\nTOP DE PRODUCTOS")
+
+    copia = productos[:]
+
+    for i in range(len(copia)-1):
+        for j in range(i+1, len(copia)):
+            if copia[j]["vendidos"] > copia[i]["vendidos"]:
+                copia[i], copia[j] = copia[j], copia[i]
+
+    limite = 3
+
+    if len(copia) < 3:
+        limite = len(copia)
+
+    for i in range(limite):
+        print(
+            f"{i+1}. {copia[i]['nombre']} - {copia[i]['vendidos']} unidades"
+        )
 
 
 def menu():
@@ -194,6 +284,7 @@ def menu():
         print("       3. Agregar al carrito")
         print("       4. Mostrar carrito")
         print("       5. Finalizar compra")
+        print("       6. Estadísticas")
         print("       0. Salir del programa")
         print("----------------------------------------")
 
@@ -222,6 +313,11 @@ def menu():
         elif opcion == "5":
             limpiar()
             finalizar()
+            sig()
+
+        elif opcion == "6":
+            limpiar()
+            estadisticas()
             sig()
 
         elif opcion == "0":
